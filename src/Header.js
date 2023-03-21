4cfc27676efc758d31d1fcb8caac5a5b91bb69a3
kingfeekra <fiachraking@gmail.com>
2023-03-21 18:14:31 +0000

    add links to header

--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,10 @@ function Header() {
       <header>
           <h1>Logo</h1>
           <ul>
-              <li>Home</li>
-              <li>Shop</li>
+              <li><a href="/">Home</a></li>
+              <li><a href="/shop">Shop</a></li>
           </ul>
-          <h1>Cart</h1>
+          <h1><a href="/cart">Cart</a></h1>
       </header>
     );
   }
